28d0cf7738a19cf72a39743470b20271118522d1
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-04 21:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const parseCode = (str) => {
-  // your code here
-};
+function parseCode(encodedString) {
+    const [firstName, lastName, id] = encodedString.split('000').filter(Boolean);
+    return {
+        firstName,
+        lastName,
+        id 
+    };
+}
+
+// Examples
+console.log(parseCode("John000Doe000123")); // Output: { firstName: "John", lastName: "Doe", id: "123" }
+console.log(parseCode("michael0smith004331")); // Output: { firstName: "michael", lastName: "smith", id: "4331" }
+console.log(parseCode("Thomas00LEE0000043")); // Output: { firstName: "Thomas", lastName: "LEE", id: "43" }
 
 // Do not change the code below
 const str = prompt("Enter str: ");
